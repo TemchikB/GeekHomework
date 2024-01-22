e857abb21da21dd77d66578482c9ecce5ba97493
Tem4k <strong.balkizov@mail.ru>
2024-01-22 10:40:35 +0300

    homeworkdone

--- dwada.py
+++ dwada.py
@@ -1,9 +1,6 @@
 number= int(input())
 phone = open('phone.txt', encoding="utf-8")
 new_phone = open('new_phone.txt','w')
-new=enumerate(phone)
-
-print(number)
 for n, line in enumerate(phone):
     if n+1 == number:
         new_phone.write(line)
